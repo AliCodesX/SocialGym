ee53d22ac63ea2ccb0ee2ace3d6f6ecd121f2094
Ali <alizadeh1412@gmail.com>
2025-02-18 23:12:01 +0100

    jaja

--- src/app/Components/CardItemDark.tsx
+++ src/app/Components/CardItemDark.tsx
@@ -1,4 +1,5 @@
-import {Card, CardHeader, CardBody, Image, Button} from "@heroui/react";
+import {Card, CardHeader, CardBody, Image, Button, Divider} from "@heroui/react";
+import {CardFooter} from "@heroui/card";
 
 export interface CardItemProps {
     image ?: string;
@@ -9,23 +10,24 @@ export interface CardItemProps {
 
 export default function CardItemDark(props: CardItemProps) {
     return (
-        <Card fullWidth={false} className="p-5 max-w-sm bg-black/50   ">
-            <CardBody className="overflow-visible py-2">
+        <Card isFooterBlurred  fullWidth={false} className="p-0 max-w-sm bg-black/50   ">
+            <CardBody className="overflow-visible py-0">
                 <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="w-full object-cover  rounded-xl"
                     src={props.image}
-                    width={600}
                     height={250}
+                    width="100%"
                 />
             </CardBody>
-            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+
+            <CardFooter className="pb-2 pt-2 px-4 flex-col items-start">
                 <h4 className="font-bold text-large text-lakers">{props.title}</h4>
                 <small className=" text-left  text-white">
                     {props.description}
                 </small>
 
-            </CardHeader>
+            </CardFooter>
         </Card>
     );
 }
